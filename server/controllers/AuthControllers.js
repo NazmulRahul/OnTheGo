541ebeaa3e6677a233bf48f7231b3619ee458483
Nazmul Hossen Rahul <69730793+nazmul-2019331047@users.noreply.github.com>
2024-10-24 12:35:01 +0600

    verification added

--- server/controllers/AuthControllers.js
+++ server/controllers/AuthControllers.js
@@ -5,27 +5,29 @@ require('dotenv').config();
 const handleLogin = async (req, res) => {
     const { user, pass } = req.body;
     if (!user || !pass) return res.status(400).json({ 'message': 'Username and password are required.' });
-    const foundUser = usersDB.users.find(person => person.username === user);
+    // const foundUser = usersDB.users.find(person => person.username === user);
+    //find user using email
     if (!foundUser) return res.sendStatus(401).json({msg:'User Not found'}); 
 
     const match = await bcrypt.compare(pass, foundUser.password);
     if (match) {
-        const roles = Object.values(foundUser.roles);
-
         const accessToken = jwt.sign(
             {
                 "UserInfo": {
                     "username": foundUser.username,
-                    "roles": roles
                 }
             },
             process.env.ACCESS_TOKEN_SECRET,
             { expiresIn: '1d' }
         );
-        res.cookie('jwt', refreshToken, { httpOnly: true, sameSite: 'None', secure: true, maxAge: 24 * 60 * 60 * 1000 });       
+        res.cookie('token', refreshToken, { httpOnly: true, sameSite: 'None', secure: true, maxAge: 24 * 60 * 60 * 1000 });       
     } else {
         res.sendStatus(401);
     }
+}
+
+const handleRegister =async()=>{
+
 }
 
 module.exports = { handleLogin };
