85735fb069ce414744e559d1d75244decc345241
Nazmul Hossen Rahul <69730793+nazmul-2019331047@users.noreply.github.com>
2024-10-24 16:32:43 +0600

    test

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,7 @@ import { Route, Routes} from 'react-router-dom'
 import ParentLayout from './components/parentLayout'
 import Login from './pages/Login'
 import Register from './pages/Register'
+import NewTrip from './components/NewTrip'
 function App() {
 
   return (
@@ -12,6 +13,8 @@ function App() {
         <Route index element={<Home/>}/>
         <Route path='login' element={<Login/>}/>
         <Route path='register' element={<Register/>}/>
+        <Route path='new-trip' element={<NewTrip/>}/>
+
       </Route>
     </Routes>
   )
