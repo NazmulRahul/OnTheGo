6fae398ab5934cc9859d8248c0eef5763c15678f
Ajoad Islam Nishat <ajoadislam@gmail.com>
2024-10-24 11:07:50 +0600

    test

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,11 @@
 import { useState } from 'react'
 import './App.css'
-
+import Home from './pages/home'
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
+      <Home/>
     </>
   )
 }
